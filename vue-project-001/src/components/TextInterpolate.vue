50c71d342b19c69dab3eb21902a4242758539815
bwire40 <emmanuelbwire80@gmail.com>
2025-02-15 14:49:54 +0300

    text-interpolation'

---
+++ vue-project-001/src/components/TextInterpolate.vue
@@ -0,0 +1,23 @@
+<!-- js -->
+<script setup>
+const product = 'socks'
+const stock = 100
+
+const add = (a, b) => {
+  return a + b
+}
+</script>
+
+<!-- html -->
+<template>
+  <p>{{ product }}</p>
+  <p>{{ stock }}</p>
+  <p>{{ add(3, 5) }}</p>
+</template>
+
+<!-- css -->
+<style scoped>
+p {
+  color: blue;
+}
+</style>
